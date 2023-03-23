4cfd3a7bd0b9c9b60e742938543b5e527e30a6a7
d3vkk <donaldkk22@yahoo.com>
2023-03-23 12:47:59 +0300

    feat(hackerrank): Solve me first

---
+++ hackerrank/solveMeFirst.js
@@ -0,0 +1,4 @@
+function solveMeFirst(a, b) {
+    return a + b;
+}
+console.log("solveMeFirst:", solveMeFirst(1, 2));
---
+++ hackerrank/solveMeFirst.ts
@@ -0,0 +1,5 @@
+function solveMeFirst(a: number, b: number): number {
+  return a + b;
+}
+
+console.log("solveMeFirst:", solveMeFirst(1, 2));
